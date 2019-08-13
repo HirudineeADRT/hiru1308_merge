230e886a5a66a178258028ac10edc564abf1db0c
HirudineeADRT <hirudinee@adroitlogic.com>
2019-08-13 15:18:39 +0530

    sns

--- hiru1308_merge/lambda.js
+++ hiru1308_merge/lambda.js
@@ -1,7 +1,24 @@
 let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
 const s3 = new AWS.S3();
 
 exports.handler = function (event, context, callback) {
+    sns.publish({
+        Message: '999',
+        MessageAttributes: {
+            'AWS.SNS.SMS.SMSType': {
+                'DataType': 'String',
+                'StringValue': 'Promotional'
+            }
+        },
+        PhoneNumber: '99999'
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
     //comment
     callback(null, { "message": "Successfully executed" });
 }

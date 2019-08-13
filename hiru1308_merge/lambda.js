f38b1310cee90d3ded30168cae869b12ee66f65e
HirudineeADRT <hirudinee@adroitlogic.com>
2019-08-13 15:13:43 +0530

    added comment

--- hiru1308_merge/lambda.js
+++ hiru1308_merge/lambda.js
@@ -2,6 +2,6 @@ let AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
 exports.handler = function (event, context, callback) {
-
+    //comment
     callback(null, { "message": "Successfully executed" });
 }
